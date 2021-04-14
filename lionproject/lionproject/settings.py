e24654bed98a9c683b369e11c1d6b6d0fff6b1de
sgwon96 <zxcvb5434@likelion.org>
2021-04-14 15:43:53 +0900

    Url & Template

--- lionproject/lionproject/settings.py
+++ lionproject/lionproject/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'lionproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['lionproject/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- lionproject/lionproject/urls.py
+++ lionproject/lionproject/urls.py
@@ -14,16 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 import blog.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', blog.views.home, name="home"),
-    path('blog/<int:blog_id>/', blog.views.detail, name="detail"),
-    path('blog/new/', blog.views.new, name='new'),
-    path('blog/create/', blog.views.create, name='create'),
-    path('blog/edit/<int:blog_id>/', blog.views.edit, name="edit"),
-    path('blog/update/<int:blog_id>/', blog.views.update, name="update"),
-    path('blog/delete/<int:blog_id>/', blog.views.delete, name="delete"),
+    path('blog/', include('blog.urls')),
 ]
