20c53152bf416523408e28b28c9915edcb31da82
sgwon96 <zxcvb5434@likelion.org>
2021-04-14 15:55:58 +0900

    introduce

--- lionproject/lionproject/urls.py
+++ lionproject/lionproject/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 import blog.views
+import introduce.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', introduce.views.home, name="home"),
     path('blog/', include('blog.urls')),
 ]
